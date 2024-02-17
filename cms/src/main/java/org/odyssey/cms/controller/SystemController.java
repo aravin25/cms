73f979273c575e8767a519105c67fdff3279f7f4
Aravin <83588782+aravin25@users.noreply.github.com>
2024-02-17 14:03:22 +0530

    Delete cms directory

--- cms/src/main/java/org/odyssey/cms/controller/SystemController.java
+++
@@ -1,7 +0,0 @@
-package org.odyssey.cms.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SystemController {
-}
--- cms/src/main/java/org/odyssey/cms/exception/AccountException.java
+++
@@ -1,7 +0,0 @@
-package org.odyssey.cms.exception;
-
-public class AccountException extends Exception {
-    public AccountException(String msg) {
-        super(msg);
-    }
-}
