1d91512059d60554e8f2938e414ed4ba4619fae5
Aravintakshan A G <aravintakshan03cse@gmail.com>
2024-02-19 17:51:40 +0530

    Implementation on Payment-Request

--- src/main/java/org/odyssey/cms/entity/PaymentRequest.java
+++ src/main/java/org/odyssey/cms/entity/PaymentRequest.java
@@ -1,6 +1,8 @@
 package org.odyssey.cms.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +14,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class PaymentRequest {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Integer paymentRequestId;
 	Integer merchantId;
 	Integer customerId;
 	Double requestAmount;
--- src/main/java/org/odyssey/cms/service/CustomerServiceImpl.java
+++ src/main/java/org/odyssey/cms/service/CustomerServiceImpl.java
@@ -3,14 +3,13 @@ package org.odyssey.cms.service;
 import org.odyssey.cms.entity.PaymentRequest;
 import org.odyssey.cms.repository.PaymentRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.AccessException;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.AccountException;
 import java.util.Optional;
 
 @Service
-public class CustomerServiceImp implements CustomerService  {
+public class CustomerServiceImpl implements CustomerService  {
 	@Autowired
 	private PaymentRequestRepository paymentRequestRepository;
 
@@ -22,7 +21,7 @@ public class CustomerServiceImp implements CustomerService  {
 			return "customer don't have any PaymentRequest";
 		}
 		else{
-			return ("customer have request form"+customerRequest.getMerchantId()+"\n amount: "+customerRequest.getRequestAmount());
+			return ("payment Request Id: "+customerRequest.getPaymentRequestId()+"\ncustomer have request form"+customerRequest.getMerchantId()+"\namount: "+customerRequest.getRequestAmount());
 		}
 	}
 }
