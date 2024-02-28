1592cb86a42ff034b8049b04d63829781fd341c6
SDORAIS2 <113187196+SurajDs-git@users.noreply.github.com>
2024-02-28 18:02:49 +0530

    DTOs updated

---
+++ src/main/java/org/odyssey/cms/dto/RequestInvoiceDTO.java
@@ -0,0 +1,15 @@
+package org.odyssey.cms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.odyssey.cms.entity.PaymentRequest;
+import org.odyssey.cms.entity.Transaction;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RequestInvoiceDTO {
+    public Transaction transaction;
+    public PaymentRequest paymentRequest;
+}
