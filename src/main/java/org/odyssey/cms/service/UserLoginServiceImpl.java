4446d7b47acf31fd73965c134cdfac0a84297b37
Aravintakshan A G <aravintakshan03cse@gmail.com>
2024-03-12 17:25:18 +0530

    Login_Logout

--- src/main/java/org/odyssey/cms/service/UserLoginServiceImpl.java
+++ src/main/java/org/odyssey/cms/service/UserLoginServiceImpl.java
@@ -6,9 +6,11 @@ import org.odyssey.cms.exception.UserException;
 import org.odyssey.cms.repository.AccountRepository;
 import org.odyssey.cms.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class UserLoginServiceImpl implements UserLoginService{
 	@Autowired
 	private UserRepository userRepository;
